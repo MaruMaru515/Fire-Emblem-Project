912a840cf285e31ce6e54a894e50421fe90fc07f
KireiMaru <noschese@bc.edu>
2016-04-25 21:47:21 -0400

    Added files via upload

---
+++ createfireemblemchars.sql
@@ -0,0 +1,3 @@
+create table FireEmblemChars (Name Varchar (50), Affiliation Varchar (50), Class Varchar (50), Attack int, Defense int, Speed int, Skill int, Magic int, Resistance int);
+Alter Table FireEmblemChars ADD ID int not null auto_increment primary key
+
