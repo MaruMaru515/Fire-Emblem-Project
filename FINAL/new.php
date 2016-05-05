be8e553b6a01796debf21c1d13e21bbff9f78281
MaruMaru515 <supannar@bc.edu>
2016-05-05 11:28:50 -0400

    Added files via upload

---
+++ FINAL/new.php
@@ -0,0 +1,8 @@
+<?php
+// $dbc = @mysqli_connect('localhost', 'noschese', 'npEN4yQJ', 'nochese') 
+// OR die("Could not connect to database: ".mysqli_connect_error());
+// $query = "select * from FireEmblemChars where ID='$champion'";
+// $result = mysqli_query ($dbc, $query) OR die('Invalid query: ' . mysqli_error($dbc));
+// $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+echo "hi there";
+?>
